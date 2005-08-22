de373742f47d4884ac325a56f63f3a1f0b98c229
Chris Lord <chrisl@src.gnome.org>
2005-08-22 11:51:52 +0000

    Name-space source files
    svn path=/trunk/; revision=25

--- src/globals.h
+++
@@ -1,9 +0,0 @@
-
-#include <glib.h>
-#include <glade/glade.h>
-#include <libebook/e-book.h>
-
-extern GladeXML *xml;
-extern GList *contacts_groups;
-extern GHashTable *contacts_table;
-extern EBook *book;
